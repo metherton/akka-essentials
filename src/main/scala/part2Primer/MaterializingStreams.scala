66445677889baafaa0001e26976ffd8ce4bffd99
metherton <martinetherton63@gmail.com>
2020-07-19 22:50:22 +0200

    WIP: using viaMat and toMat // very important

--- src/main/scala/part2Primer/MaterializingStreams.scala
+++ src/main/scala/part2Primer/MaterializingStreams.scala
@@ -46,4 +46,38 @@ object MaterializingStreams extends App {
   // both ways
   Flow[Int].map(x => 2 * x).runWith(simpleSource, simpleSink)
 
+  /**
+    * Exercises..
+    * return the last element out of a source (use Sink.last)
+    * Compute the word count out of a stream of sentences
+    * use map op on flow, fold op on flow and sink, reduce op on flow and sink
+    */
+
+  val f1 = Source(1 to 10).toMat(Sink.last)(Keep.right).run()
+  val f2 = Source(1 to 10).runWith(Sink.last)
+
+  val sentenceSource = Source(List(
+    "Akka is awesome",
+    "I love streams",
+    "Materialized values are killing me"
+  ))
+  val wordCountSink = Sink.fold[Int, String](0)((currentWords, newSentence) => currentWords + newSentence.split(" ").length)
+  val g1 = sentenceSource.toMat(wordCountSink)(Keep.right).run()
+  val g2 = sentenceSource.runWith(wordCountSink)
+  val g3 = sentenceSource.runFold(0)((currentWords, newSentence) => currentWords + newSentence.split(" ").length)
+
+  val wordCountFlow = Flow[String].fold[Int](0)((currentWords, newSentence) => currentWords + newSentence.split(" ").length)
+  val g4 = sentenceSource.via(wordCountFlow).toMat(Sink.head)(Keep.right).run()
+  val g5 = sentenceSource.viaMat(wordCountFlow)(Keep.left).toMat(Sink.head)(Keep.right).run()
+  val g6 = sentenceSource.via(wordCountFlow).runWith(Sink.head)
+  val g7 = wordCountFlow.runWith(sentenceSource, Sink.head)._2
+
+  // this is my solution
+  val mappedSource = sentenceSource.map(sentence => sentence.split(" "))
+  val flowFoldCount = Flow[Array[String]].fold(0)((count, sentenceWords) => count + sentenceWords.length)
+  val wordCountFlow2 = mappedSource.viaMat(flowFoldCount)(Keep.left).runWith(Sink.head)
+  wordCountFlow2.onComplete {
+    case Success(value) => println(s"wordCount is : $value")
+    case Failure(ex) => println(s"Unable to get word count: $ex")
+  }
 }
