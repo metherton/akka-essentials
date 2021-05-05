4049a960df73554f03d6b51386390742ddaf957f
metherton <martinetherton63@gmail.com>
2021-05-05 23:36:54 +0200

    test kits

--- src/main/scala/part3testing/BasicSpec.scala
+++ src/main/scala/part3testing/BasicSpec.scala
@@ -3,7 +3,9 @@ package part3testing
 import akka.actor.{Actor, ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestKit}
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
+
 import scala.concurrent.duration._
+import scala.util.Random
 
 object BasicSpec {
 
@@ -19,7 +21,13 @@ object BasicSpec {
   }
 
   class LabTestActor extends Actor {
+    val random = new Random()
     override def receive: Receive = {
+      case "greeting" =>
+        if (random.nextBoolean()) sender() ! "hi" else sender() ! "hello"
+      case "favoriteTech" =>
+        sender() ! "Scala"
+        sender() ! "Akka"
       case message: String => sender() ! message.toUpperCase
     }
   }
@@ -58,10 +66,37 @@ class BasicSpec extends TestKit(ActorSystem("BasicSpec"))
   // message assertions
   "A lab test actor" should {
     val labTestActor = system.actorOf(Props[LabTestActor])
+
     "turn a string into upper case" in {
       labTestActor ! "I love akka"
       val reply = expectMsgType[String]
       assert( reply == "I LOVE AKKA")
+    }
+
+    "reply to a greeting" in {
+      labTestActor ! "greeting"
+      expectMsgAnyOf("hi", "hello")
+    }
+
+    "reply with favorite tech" in {
+      labTestActor ! "favoriteTech"
+      expectMsgAllOf("Scala", "Akka")
+    }
+
+    "reply with cool tech in a different way" in {
+      labTestActor ! "favoriteTech"
+      val messages = receiveN(2) // Seq[Any]
+
+      // free to do more complicated assertions
+    }
+
+    "reply with cool tech in a fancy way" in {
+      labTestActor ! "favoriteTech"
+
+      expectMsgPF() {
+        case "Scala" => // only care that the PF is defined
+        case "Akka" =>
+      }
     }
   }
 
